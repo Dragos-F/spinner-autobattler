2c7b43994b035e398c833ef623d00e9965c6adf1
JQHY <jonathan.q.yeap@durham.ac.uk>
2026-06-22 15:47:57 +0100

    Damage/Healing functions, generic health text display

--- Scripts/hp_entity.gd
+++ Scripts/hp_entity.gd
@@ -4,10 +4,13 @@ extends Node2D
 @export var currenthealth:float = 100
 
 @export var animator:AnimationPlayer
+@export var healthtext:Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animator.play("hp_progress")
+	updateHealthDisplay()
+	animator.speed_scale = 0
 	pass # Replace with function body.
 
 var currentstate:float = -1
@@ -15,6 +18,39 @@ var currentstate:float = -1
 func _process(delta: float) -> void:
 	if maxhealth <=0:
 		maxhealth = 1
+	pass
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_A:
+			damage(1)
+		elif event.keycode == KEY_D:
+			heal(1)
+		elif event.keycode == KEY_RIGHT:
+			position += Vector2(5,0)
+		elif event.keycode == KEY_LEFT:
+			position += Vector2(-5,0)
+			
+func damage(amount:float)->float:
+	var endhealth = changehealth(-amount)
+	return endhealth
+	
+func heal(amount:float)->float:
+	var endhealth = changehealth(amount)
+	return endhealth
+	
+func changehealth(amount:float)->float:
+	currenthealth = currenthealth+amount
+	if currenthealth > maxhealth:
+		currenthealth = maxhealth
+	if currenthealth < 0:
+		currenthealth = 0
+	updateHealthDisplay()
+	return currenthealth
+	
+func updateHealthDisplay()->void:
+	if healthtext:
+		healthtext.text = str(int(round(currenthealth)))+"/"+str(int(round(maxhealth)))
 	var healthfraction = currenthealth/maxhealth
 	if currentstate != healthfraction:
 		print("updating health blend to "+str(healthfraction))
@@ -23,20 +59,3 @@ func _process(delta: float) -> void:
 		animator.speed_scale = 0
 		currentstate = healthfraction
 	pass
-	
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_A:
-			if currenthealth-1<0:
-				currenthealth = 0
-			else:
-				currenthealth = currenthealth -1
-		elif event.keycode == KEY_D:
-			if currenthealth+1 > maxhealth:
-				currenthealth = maxhealth
-			else:
-				currenthealth = currenthealth+1
-		elif event.keycode == KEY_RIGHT:
-			position += Vector2(5,0)
-		elif event.keycode == KEY_LEFT:
-			position += Vector2(-5,0)
