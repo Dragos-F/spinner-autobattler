d20b5ab621ebff54c9d140debc0256342d6b028b
JQHY <jonathan.q.yeap@durham.ac.uk>
2026-06-26 00:11:35 +0100

    Update item_slot.gd

--- Scripts/item_slot.gd
+++ Scripts/item_slot.gd
@@ -8,8 +8,11 @@ enum Type {Loot,Inv,Bin}
 @export var old_anchor:Control
 @export var equipped_item:Draggable
 @export var hovering:bool = false
+<<<<<<< Updated upstream
 @export var once:bool = true
 @export var type:Type
+=======
+>>>>>>> Stashed changes
 
 @export_category("Wheel Info")
 @export var slot_number:int
@@ -21,22 +24,25 @@ func _ready() -> void:
 	equipped_item = null
 	
 func _process(delta: float) -> void:
-	var local_rect:Rect2 = get_global_rect()
-	if local_rect.has_point(get_global_mouse_position()) && once && manager.holding:
-		print("rect hovering")
-		reassign_anchor()
-		once = false
-		hovering = true
-		
+	pass
 
 
 func _on_mouse_entered() -> void:
 	hovering = true
 	print("Hovering over slot")
 	if manager.holding:
-		reassign_anchor()
+		#print(manager.held_object.item)
+		old_anchor = manager.held_object.anchor_pos
+		reset_size_preview(old_anchor)
+		old_anchor.equipped_item = null
+		equipped_item = manager.held_object
+		manager.held_object.anchor_pos = self
+		increase_size_preview(self)
+		#print(manager.held_object.anchor_pos)
+		#print(manager.held_object.item)
 	
 func _input(event: InputEvent) -> void:
+<<<<<<< Updated upstream
 	if event.is_action_released("MouseLeft") and !event.is_echo():
 		if hovering:
 			if manager.held_object!=null:
@@ -53,10 +59,14 @@ func _input(event: InputEvent) -> void:
 				manager.loot_sys._on_skip_loot_pressed()
 			
 	
+=======
+	if event.is_action_released("MouseLeft"):
+		reset_size_preview(self)
+		manager.update_wheels()
+>>>>>>> Stashed changes
 
 func _on_mouse_exited() -> void:
 	hovering = false
-	once = true
 	
 func increase_size_preview(target:Control):
 	target.scale += Vector2(0.1,0.1)
@@ -64,6 +74,7 @@ func increase_size_preview(target:Control):
 func reset_size_preview(target:Control):
 	target.scale = Vector2.ONE
 	
+<<<<<<< Updated upstream
 func reassign_anchor():
 		if equipped_item == null or manager.held_object.item.isUpgrade && equipped_item != null:
 			old_anchor = manager.held_object.anchor_pos
@@ -85,3 +96,6 @@ func reassign_anchor():
 		#print ("EQUIPPED " +str(equipped_item))
 		#print(manager.held_object.anchor_pos)
 		#print(manager.held_object.item)
+=======
+	
+>>>>>>> Stashed changes
