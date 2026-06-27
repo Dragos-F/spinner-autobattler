86fb8427ee4bd212b01dd91be5e07d148dac7288
JQHY <jonathan.q.yeap@durham.ac.uk>
2026-06-27 11:57:45 +0100

    Start Menu options windows

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -2,12 +2,13 @@ extends Node
 
 @export var crt_bars:Node2D
 @export var track:AudioStream
+@export var bonusfeatureswindow:Control
+@export var optionsmenuwindow:Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AudioBrain.music.stream = track
 	AudioBrain.music.play()
-
-
+	_on_backtomenu_pressed()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -21,3 +22,16 @@ func _on_play_pressed() -> void:
 	await Fader.fade_finished
 	get_tree().change_scene_to_file("res://Scenes/CharacterSelect.tscn")
 	Fader.FadeDown("")
+
+func _on_backtomenu_pressed() -> void:
+	print("back to main screen")
+	bonusfeatureswindow.visible = false
+	optionsmenuwindow.visible = false
+
+func _on_bonusbutton_pressed() -> void:
+	print("show bonus features")
+	bonusfeatureswindow.visible = true
+	
+func _on_options_pressed() -> void:
+	print("show options")
+	optionsmenuwindow.visible = true
